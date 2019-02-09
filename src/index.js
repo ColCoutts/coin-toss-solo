46f961d56e84e869db97ec2206ae1224e15c63e2
COLIN COUTTS <COUTTSTCOLIN@GMAIL.COM>
2019-02-08 21:09:26 -0800

    created win loss tally function

--- src/index.js
+++ src/index.js
@@ -2,36 +2,43 @@ import catOrDog from './cat-or-dog.js';
 
 const image = document.getElementById('animal');
 const flipForm = document.getElementById('flip-form');
-let winText = document.getElementById('result-board');
 let loseText = document.getElementById('result-board');
+let winText = document.getElementById('result-board');
 
+let clicks = 0;
+let clickCount = document.getElementById('wins').innerText = clicks;
+let loseCount = document.getElementById('losses').innerText = clicks;
 
 flipForm.addEventListener('submit', function(event){
     event.preventDefault();
-    
     const randomNumber = Math.random();
     
     let animal = catOrDog(randomNumber); 
     let imageSource = '';
-    
     if(animal === 'cat'){
         imageSource = '../assets/cat.jpg';
-        console.log('cat');
+        // console.log('cat');
     } else {
         imageSource = '../assets/dog.jpg';
-        console.log('dog');
+        // console.log('dog');
     }
     
     image.src = imageSource;
     image.classList.remove('hidden');
     
     const userChoice = flipForm.elements.animal.value;
+    
 
     if(userChoice === animal){
         winText.textContent = 'winner';
+        clickCount += 1;
+        document.getElementById('wins').innerText = clickCount;
+        console.log(clickCount);
     } else {
+        loseCount += 1;
+        document.getElementById('losses').innerText = loseCount;
         loseText.textContent = 'loser';
     }
 
-})
+});
 
